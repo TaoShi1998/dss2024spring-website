7329970221c4cbfd9a4284afad7dc54a210c441e
Tao Shi <u6584092@anu.edu.au>
2024-04-02 00:14:22 +0800

    Delete dss2024spring-website directory

--- dss2024spring-website/README.md
+++
@@ -1,9 +0,0 @@
-# dss2023spring-website
-Class Website for Data Science and Information Technology Seminar (Spring 2023)
-
-To generate html pages, go to `jemdoc_files/`, run
-
-```
-jemdoc -c mysite.conf -o ../www/ *.jemdoc
-```
-
--- dss2024spring-website/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# deploy.sh
-# This script deploys the website content to the web server 
-# Note: configured to run on the server only
-
-cp -r www/* /var/www/wordpress.loc/home/classes/dss2023spring/.
-echo "done!"
